fa2bc8dc6794b24bf228cad7690db80034abb433
kemaltt <ttnckemal@gmail.com>
2022-04-26 17:56:11 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const text = document.getElementById('text')
+
+function formSubmit() {
+
+    console.log(name.value);
+    console.log(email.value);
+    console.log(text.value);
+
+}
