3d3d28e08a63456e4a90b29c42ea444cbb54b8bc
kemaltt <ttnckemal@gmail.com>
2022-04-26 20:26:17 +0200

    nav-styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
 const name = document.getElementById('name')
 const email = document.getElementById('email')
 const text = document.getElementById('text')
+const slideNav = document.getElementById('slide-in-nav')
+const tabNav = document.getElementById('tab-nav')
 
 function formSubmit() {
 
     console.log(name.value);
     console.log(email.value);
     console.log(text.value);
+    name.value = ''
+    email.value = ''
+    text.value = ''
+}
 
+const showNav = () => {
+    slideNav.style.left = '70%';
+    slideNav.style.right = 0;
+    tabNav.style.visibility = 'hidden'
+}
+
+const closeNav = () => {
+    slideNav.style.left = '100%'
+    slideNav.style.right = '-100%'
+    tabNav.style.visibility = 'visible'
 }
